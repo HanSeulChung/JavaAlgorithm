e7df2ffc8d778542001a2c9ee8b74a8a93636608
hanseul <w0w1278@naver.com>
2023-06-14 19:00:51 +0900

    6/14

--- src/com/codingtest/backjoon/BJ10818.java
+++ src/com/codingtest/backjoon/BJ10818.java
@@ -2,12 +2,35 @@ package com.codingtest.backjoon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class BJ10818 {
-    public static void solution(){
 
-    }
     public static void main(String[] args) {
+        int[] answer = new int[2];
+        int N =0;
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        int[] intArr = new int[N];
+        for(int i=0; i<N; i++){
+            intArr[i] = sc.nextInt();
+        }
+        int min = intArr[0];
+        int max = intArr[0];
+        for(int num: intArr){
+            if(min>num){
+                min = num;
+            }
+            if(max<num){
+                max = num;
+            }
+        }
+        answer[0] = min;
+        answer[1] = max;
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(min+" "+max);
 
+        Arrays.sort(intArr);
+        System.out.println(intArr[0] + " " +intArr[N-1]);
     }
 }
--- src/com/study/ArrayStudy.java
+++ src/com/study/ArrayStudy.java
@@ -16,45 +16,62 @@ public class ArrayStudy {
         int[] intArr6 = new int[0];
 
         //int arr 출력은 for-each 방법도 있으나 간편하게 해당 출력으로만 대체함
-        System.out.println("Array: int[]");
-        System.out.println(Arrays.toString(intArr1));
+        System.out.println("=== Array: int[] ===");
+        System.out.println("intArr1: "+ Arrays.toString(intArr1));
         System.out.println("intArr1 길이: "+intArr1.length);
-        System.out.println(Arrays.toString(intArr2));
-        System.out.println(Arrays.toString(intArr3));
-        System.out.println(Arrays.toString(intArr4));
-        System.out.println(Arrays.toString(intArr5));
-        System.out.println(Arrays.toString(intArr6));
+        System.out.println("intArr2: "+ Arrays.toString(intArr2));
+        System.out.println("intArr3: "+ Arrays.toString(intArr3));
+        System.out.println("intArr4: "+ Arrays.toString(intArr4));
+        System.out.println("intArr5: "+ Arrays.toString(intArr5));
+        System.out.println("intArr6: "+ Arrays.toString(intArr6));
         System.out.println("intArr6 길이: "+intArr6.length);
 
-//        intArr6[0] = 10;
+//        intArr6[0] = 10; //크기가 없는 배열에는 값을 넣을 수 없다. 인덱스 오류가 날 것이다.
 //        System.out.println(Arrays.toString(intArr6));
 
         intArr5[0] = 10;
-        System.out.println(Arrays.toString(intArr5));
-//        intArr5[1] = 20;
+        System.out.println("intArr5[0] =10 한 후 intArr5: " + Arrays.toString(intArr5));
+//        intArr5[1] = 20; // 크기가 1인 배열에는 인덱스가 1부터는 존재하지 않음. 마찬가지로 넣을 수 없음. 인덱스 오류가 날 것이다.
 //        System.out.println(Arrays.toString(intArr5));
 
         System.out.println();
-        System.out.println("Array: String[]");
+        System.out.println("=== Array: String[] ===");
         String[] stringArr1 = new String[5];
+        System.out.println("stringArr1: "+ Arrays.toString(stringArr1));
         stringArr1[0] = "Hi";
         String[] stringArr2=new String[]{"Kim","Park","Yi"}; // new String[] 생략가능
 
-        System.out.println(Arrays.toString(stringArr1));
-        System.out.println(Arrays.toString(stringArr2));
+        System.out.println("stringArr1[0] = \"Hi\" 한 후 stringArr1: "+ Arrays.toString(stringArr1));
+        System.out.println("stringArr2: "+ Arrays.toString(stringArr2));
 
         System.out.println();
-        System.out.println("Array: char[]");
-        char[] charArr = {'a', 'b', 'c', 'd', 'e'};
-        System.out.println(charArr);  //char array를 println할 경우 println의 고유적인 기능
+        System.out.println("=== Array: char[]===");
+        char[] charArr1 ={};
+        char[] charArr2 = {'a', 'b', 'c', 'd', 'e'};
 
+        System.out.print("charArr1: ");
+        System.out.println(charArr1);
+        System.out.print("charArr2: ");
+        System.out.println(charArr2);               //char array만 println할 경우 println의 고유적인 기능
+        System.out.println("charArr2: "+ charArr2); // 다른것들이 추가되면 x
+        System.out.println();
+
+
+        System.out.println("=== Array: boolean[]===");
+        boolean[] booleanArr1 ={};
+        boolean[] booleanArr2 = new boolean[2];
+        System.out.println("booleanArr1: "+ Arrays.toString(booleanArr1));
+        System.out.println("booleanArr2: "+ Arrays.toString(booleanArr2));
+        System.out.println();
         //Collection 프레임 워크의 일부인 ArrayList
         ArrayList<Integer> IntegerArrayList = new ArrayList<>();
-        System.out.println(IntegerArrayList);
+        System.out.println("=== ArrayList : ArrayList<Integer> ===");
+        System.out.println("ArrayList is not equal Array, is List");
+        System.out.println("IntegerArrayList: "+ IntegerArrayList);
         IntegerArrayList.add(3);
-        System.out.println(IntegerArrayList);
+        System.out.println("IntegerArrayList: "+ IntegerArrayList);
         IntegerArrayList.add(5);
         IntegerArrayList.add(3);
-        System.out.println(IntegerArrayList);
+        System.out.println("IntegerArrayList: "+ IntegerArrayList);
     }
 }

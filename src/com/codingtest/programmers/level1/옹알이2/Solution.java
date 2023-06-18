03c0a26ef45655ade3ceeda0542efd36286b3044
hanseul <w0w1278@naver.com>
2023-06-18 22:29:59 +0900

    6/18

--- src/com/codingtest/programmers/level1/옹알이2/Solution.java
+++ src/com/codingtest/programmers/level1/옹알이2/Solution.java
@@ -1,8 +1,30 @@
 package com.codingtest.programmers.level1.옹알이2;
 
 public class Solution {
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        for(int i = 0; i < babbling.length; i++) {
+            if(babbling[i].contains("ayaaya") || babbling[i].contains("yeye") || babbling[i].contains("woowoo") || babbling[i].contains("mama")) {
+                continue;
+            }
+
+            babbling[i] = babbling[i].replace("aya", " ");
+            babbling[i] = babbling[i].replace("ye", " ");
+            babbling[i] = babbling[i].replace("woo", " ");
+            babbling[i] = babbling[i].replace("ma", " ");
+            babbling[i] = babbling[i].replace(" ", "");
+
+            if(babbling[i].length()  == 0) answer++;
+
+        }
+        return answer;
+    }
 
     public static void main(String[] args) {
+        String[] babbling = {"aya", "yee", "u", "maa"};
+        System.out.println(solution(babbling));
 
+        babbling = new String[]{"ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"};
+        System.out.println(solution(babbling));
     }
 }

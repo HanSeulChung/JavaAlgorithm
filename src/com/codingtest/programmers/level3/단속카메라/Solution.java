6f805cdd9acd49843970b49f4a14ae578004a8d0
HanSeul <w0w1278@naver.com>
2023-07-14 16:34:36 +0900

    7/14

--- src/com/codingtest/programmers/level3/단속카메라/Solution.java
+++ src/com/codingtest/programmers/level3/단속카메라/Solution.java
@@ -6,10 +6,18 @@ public class Solution {
 
     public static int solution(int[][] routes) {
         int answer = 0;
-        Arrays.sort(routes, (a, b) -> Integer)
+        Arrays.sort(routes, (a, b) -> Integer.compare(a[1] , b[1]));
+        int min = Integer.MIN_VALUE;
+        for (int[] route : routes){
+            if(min < route[0]){
+                min = route[1];
+                answer++;
+            }
+        }
         return answer;
     }
     public static void main(String[] args) {
-
+        int[][] routes = {{-20,-15}, {-14,-5}, {-18,-13}, {-17,-3}};
+        System.out.println(solution(routes));
     }
 }

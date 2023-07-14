d257a9627947870eaac06d3fe0986567b8506929
HanSeul <w0w1278@naver.com>
2023-07-14 10:38:08 +0900

    7/14

--- src/com/surpriseassignment/Assign2.java
+++ src/com/surpriseassignment/Assign2.java
@@ -23,34 +23,38 @@ class Point {
 }
 
 public class Assign2{
-    static int mindistance = Integer.MAX_VALUE;
+    static float mindistance = Integer.MAX_VALUE;
     static Point mindistancePoint = new Point(Integer.MIN_VALUE, Integer.MIN_VALUE);
     static Point myPoint;
     public static void calDistance(Point randompoint){
-        int tmpdistance = (int)Math.sqrt((myPoint.x - randompoint.x)*(myPoint.x - randompoint.x) + (myPoint.y - randompoint.y)*(myPoint.y - randompoint.y));
+        float tmpdistance = (float)Math.sqrt((myPoint.x - randompoint.x)*(myPoint.x - randompoint.x) + (myPoint.y - randompoint.y)*(myPoint.y - randompoint.y));
+        System.out.printf("(%d, %d) => %.6f\n", randompoint.x, randompoint.y, tmpdistance);
         if(mindistance > tmpdistance){
             mindistance = tmpdistance;
             mindistancePoint = new Point(randompoint.x, randompoint.y);
         }
     }
     public static void main(String[] args) throws IOException {
-        System.out.print("나의 좌표 값을 입력하세요. 입력 순서대로 x, y값이 저장됩니다. 예시)3 2 : ");
+        System.out.println("내 좌표 x값을 입력해 주세요.");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int myX = Integer.parseInt(st.nextToken());
+        System.out.println("내 좌표 y값을 입력해 주세요.");
+        st = new StringTokenizer(br.readLine());
         int myY = Integer.parseInt(st.nextToken());
         myPoint = new Point(myX, myY);
 
         ArrayList<Point> list = new ArrayList<>();
-        System.out.println();
-        System.out.println("임의의 좌표 값을 총 10개 입력하세요. 입력 순서대로 x, y값이 저장됩니다.");
-
         for (int i = 1; i <= 10; i++){
-            System.out.printf("좌표 %d: ", i);
+            System.out.printf("%d/10 번째 입력\n", i);
+            System.out.println("임의의 좌표 x값을 입력해 주세요.");
             st = new StringTokenizer(br.readLine());
             int X = Integer.parseInt(st.nextToken());
+            System.out.println("임의의 좌표 y값을 입력해 주세요.");
+            st = new StringTokenizer(br.readLine());
             int Y = Integer.parseInt(st.nextToken());
             if(list.contains(new Point(X, Y))) {
+                System.out.println("동일한 좌표값이 이미 존재합니다. 다시 입력해 주세요.");
                 i--;
             } else {
                 list.add(new Point(X, Y));
@@ -60,7 +64,8 @@ public class Assign2{
         for (int i = 0; i < list.size(); i++){
             calDistance(list.get(i));
         }
-        System.out.printf("나와 가장 가까운 좌표값은 (%d, %d)입니다.", mindistancePoint.x, mindistancePoint.y);
+        System.out.println("제일 가까운 좌표:");
+        System.out.printf("(%d, %d) => %.6f\n", mindistancePoint.x, mindistancePoint.y, mindistance);
     }
 }
 

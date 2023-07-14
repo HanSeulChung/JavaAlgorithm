77077d12b9169b459c08efff96955d569414727c
HanSeul <w0w1278@naver.com>
2023-07-14 15:37:17 +0900

    7/17 property.html 삭제

---
+++ src/com/codingtest/programmers/level3/Solution.java
@@ -0,0 +1,4 @@
+package com.codingtest.programmers.level3;
+
+public class Solution {
+}
---
+++ src/com/codingtest/programmers/level3/단속카메라/Solution.java
@@ -0,0 +1,15 @@
+package com.codingtest.programmers.level3.단속카메라;
+// 그리디
+
+import java.util.*;
+public class Solution {
+
+    public static int solution(int[][] routes) {
+        int answer = 0;
+        Arrays.sort(routes, (a, b) -> Integer)
+        return answer;
+    }
+    public static void main(String[] args) {
+
+    }
+}

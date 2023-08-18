c476ce9d0b640f5ff87b1b029370a1dda143b760
HanSeul <w0w1278@naver.com>
2023-08-18 16:31:42 +0900

    8/18 6주차 코딩테스트 답안 확인

--- src/com/codingtest/zerobaseshool/코딩테스트6주차/P2.java
+++ src/com/codingtest/zerobaseshool/코딩테스트6주차/P2.java
@@ -1,2 +1,52 @@
-package com.codingtest.zerobaseshool.코딩테스트6주차;public class P2 {
+package com.codingtest.zerobaseshool.코딩테스트6주차;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class P2 {
+    public int solution(int[] amount, int[] value, int[] stomach) {
+        List<Meat> meats = IntStream.range(0, amount.length)
+                .boxed()
+                .map(i -> new Meat(value[i], amount[i]))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+        int bestMeatConsumption =
+                Math.min(meats.get(0).amount / stomach.length,
+                        Arrays.stream(stomach).min().getAsInt()) * stomach.length;
+        int totalStomach = Arrays.stream(stomach).sum() - bestMeatConsumption;
+        int result = bestMeatConsumption * meats.get(0).value;
+
+        for (int i = 1; i < meats.size(); i++) {
+            int v = meats.get(i).value;
+            int a = meats.get(i).amount;
+            int consumption = Math.min(totalStomach, a);
+            result += v * consumption;
+            totalStomach -= consumption;
+            if (totalStomach == 0) {
+                break;
+            }
+        }
+        return result;
+    }
+
 }
+
+class Meat implements Comparable<Meat> {
+    int value;
+    int amount;
+
+    public Meat(int value, int amount) {
+        this.value = value;
+        this.amount = amount;
+    }
+
+    @Override
+    public int compareTo(Meat o) {
+        return this.value - o.value;
+    }
+}
+
--- src/com/codingtest/zerobaseshool/코딩테스트6주차/P5.java
+++ src/com/codingtest/zerobaseshool/코딩테스트6주차/P5.java
@@ -1,2 +1,40 @@
-package com.codingtest.zerobaseshool.코딩테스트6주차;public class P5 {
+package com.codingtest.zerobaseshool.코딩테스트6주차;
+
+import java.util.Arrays;
+public class P5 {
+    public int[] mysolution(int[] scores) {
+        int n = scores.length;
+        int[] answer = new int[n];
+        Arrays.fill(answer, 1);
+
+        for (int i = 1; i < n; i++) {
+            if (scores[i] > scores[i - 1]) {
+                answer[i] = answer[i - 1] + 1;
+            }
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            if (scores[i] > scores[i + 1] && answer[i] <= answer[i + 1]) {
+                answer[i] = answer[i + 1] + 1;
+            }
+        }
+
+        return answer;
+    }
+    public int[] solution(int[] scores) {
+        int[] chocos = new int[scores.length];
+        Arrays.fill(chocos, 1);
+
+        for (int i = 0; i < scores.length - 1; i++) {
+            if (scores[i + 1] > scores[i]) {
+                chocos[i + 1] = chocos[i] + 1;
+            }
+        }
+
+        for (int i = scores.length - 2; i >= 0; i--) {
+            if (scores[i + 1] < scores[i]) {
+                chocos[i] = Math.max(chocos[i + 1] + 1, chocos[i]);
+            }
+        }
+        return chocos;
+    }
 }

a5df8829009aa75e79bcfd655d7de60a6efc11ff
HanSeul <w0w1278@naver.com>
2023-09-20 16:20:06 +0900

    안전지대, 전국 대회 선발

--- src/com/codingtest/programmers/level0/안전지대/Solution.java
+++ src/com/codingtest/programmers/level0/안전지대/Solution.java
@@ -1,6 +1,85 @@
 package com.codingtest.programmers.level0.안전지대;
 
 public class Solution {
+
+    public void dfs1(int x, int y, int[][] board) {
+
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n) {
+                if (!visited[nextX][nextY]) {
+                    visited[nextX][nextY] = true;
+                    answer++;
+                }
+            }
+        }
+    }
+
+    public int solution1(int[][] board) {
+        answer = 0;
+        n = board.length;
+        visited = new boolean[n][n];
+
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    if (!visited[i][j]) {
+                        answer++;
+                    }
+                    dfs(i, j, board);
+                }
+            }
+        }
+
+        return n * n - answer;
+    }
+    static int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+    static int[] dy = {1, 0 , -1, 1, -1, 1, 0, -1};
+    static boolean[][] visited;
+    static int answer;
+    static int n;
+
+    public void dfs(int x, int y, int[][] board) {
+
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nextX = x + dx[i];
+            int nextY = y + dy[i];
+            if (nextX >= 0 && nextX < n && nextY >= 0 && nextY < n) {
+                if (!visited[nextX][nextY]) {
+                    visited[nextX][nextY] = true;
+                }
+            }
+        }
+    }
+
+    public int mySolution(int[][] board) {
+        answer = 0;
+        n = board.length;
+        visited = new boolean[n][n];
+
+        for (int i = 0 ; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) {
+                    dfs(i, j, board);
+                }
+            }
+        }
+
+        for (int j = 0 ; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                if (!visited[j][i]) {
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
     public int solution(int[][] board) {
         int answer = 0;
         int N = board.length;
--- src/com/codingtest/programmers/level0/전국대회선발고사/Solution.java
+++ src/com/codingtest/programmers/level0/전국대회선발고사/Solution.java
@@ -2,7 +2,29 @@ package com.codingtest.programmers.level0.전국대회선발고사;
 
 import java.util.*;
 class Solution {
-    public int solution(int[] rank, boolean[] attendance) {
+    public int mySolution2(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        int cnt = 2;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < attendance.length; i++) {
+            if (attendance[i]) {
+                map.put(rank[i], i);
+            }
+        }
+
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+
+        int idx = 0;
+        while (cnt >= 0) {
+            answer += (int) Math.pow(100, cnt) * map.get(list.get(idx++));
+            cnt--;
+        }
+
+        return answer;
+    }
+    public int mySolution1(int[] rank, boolean[] attendance) {
         int answer = 0;
         Map<Integer, Boolean> checkAttendance = new HashMap<>();
 

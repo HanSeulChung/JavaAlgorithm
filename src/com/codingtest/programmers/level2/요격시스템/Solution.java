552b090b06b70bc1f913ef2b8e2e6c855443c483
HanSeul <w0w1278@naver.com>
2024-02-03 22:12:16 +0900

    2월 3일(토) 요격 시스템 복습
    자료구조 PQ로 사용

--- src/com/codingtest/programmers/level2/요격시스템/Solution.java
+++ src/com/codingtest/programmers/level2/요격시스템/Solution.java
@@ -5,6 +5,32 @@ import java.util.*;
 // 그리디
 
 public class Solution {
+  public int solutionPQ(int[][] targets) {
+    PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> {
+      if (o1[1] == o2[1]) {
+        return o2[0] - o1[0];
+      }
+
+      return o1[1] - o2[1];
+    });
+
+    for (int[] target : targets) {
+      pq.offer(target);
+    }
+
+    int[] missile = pq.poll();
+    int answer = 1;
+    while(!pq.isEmpty()) {
+      if (missile[1] > pq.peek()[0]) {
+        pq.poll();
+      } else {
+        missile = pq.poll();
+        answer++;
+      }
+    }
+
+    return answer;
+  }
   public int solution(int[][] targets) {
     int answer = 0;
 
